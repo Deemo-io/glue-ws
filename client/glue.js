a6108d8b84f585fa253f8a5b7ac6c0e6836e2755
Zack <zseliger@gmail.com>
2018-07-16 23:35:39 -0500

    added a big indent

--- client/glue.js
+++ client/glue.js
@@ -1,158 +1,158 @@
 //GLUE.js client
 var GlueClient = function(options={updateFPS: 60}) {
-var GLUE = {ws: null, frames: [], frameTimes: [], currentTime: 0, prevTime: 0, dt: 0, instances: [], classes: new Map(), packetFunctions: new Map(), objects: [], packetsToSend: [], time: 0};
-GLUE.updateFPS = options.updateFPS;
-GLUE.updateTime = (1/GLUE.updateFPS)*1000;
+	var GLUE = {ws: null, frames: [], frameTimes: [], currentTime: 0, prevTime: 0, dt: 0, instances: [], classes: new Map(), packetFunctions: new Map(), objects: [], packetsToSend: [], time: 0};
+	GLUE.updateFPS = options.updateFPS;
+	GLUE.updateTime = (1/GLUE.updateFPS)*1000;
 
-GLUE.init = function(ip) {
-	GLUE.ws = new WebSocket(ip);
-	GLUE.addPacketFunction("u", GLUE.updateObjects);
-	GLUE.addPacketFunction("a", GLUE.addObject);
-	GLUE.addPacketFunction("r", GLUE.removeObject);
-	
-	GLUE.ws.onopen = function() {
+	GLUE.init = function(ip) {
+		GLUE.ws = new WebSocket(ip);
+		GLUE.addPacketFunction("u", GLUE.updateObjects);
+		GLUE.addPacketFunction("a", GLUE.addObject);
+		GLUE.addPacketFunction("r", GLUE.removeObject);
 		
-	}
-	GLUE.ws.onmessage = function(message) {
-		var data = JSON.parse(message.data);
-		
-		if (GLUE.frames.length < 1) {
-			GLUE.executeFrame(data.m);
-		}
-		GLUE.frames.push(data.m);
-		GLUE.frameTimes.push(data.t);
-	}
-}
-
-GLUE.executeFrame = function(messages) {
-	for (var i = 0; i < messages.length; i++) {
-		var func = GLUE.packetFunctions.get(messages[i].t);
-		if (func !== undefined)
-			func(messages[i].d);
-		else
-			console.log("Packet function for " + messages[i].t + " is undefined");
-	}
-}
-
-GLUE.update = function() {
-	GLUE.prevTime = GLUE.currentTime;
-	GLUE.currentTime = Date.now();
-	GLUE.dt = (GLUE.currentTime-GLUE.prevTime) / GLUE.updateTime;
-	
-	if (GLUE.packetsToSend.length > 0 && GLUE.ws.readyState == 1) {
-		GLUE.ws.send(JSON.stringify(GLUE.packetsToSend));
-		GLUE.packetsToSend = [];
-	}
-	
-	if (GLUE.frameTimes[0] == undefined || GLUE.frameTimes[1] == undefined)
-		return;
-	
-	var tick = (GLUE.currentTime - GLUE.prevTime) * GLUE.dt / (GLUE.frameTimes[1] - GLUE.frameTimes[0]);
-	GLUE.time += tick;
-	
-	if (GLUE.time >= 1.0) {
-		while (GLUE.frameTimes.length > 2) {
-			//advance to next frame
-			GLUE.frames.splice(0, 1);
-			GLUE.frameTimes.splice(0, 1);
-			GLUE.time -= 1.0;
-			//execute all of next frame's functions
-			GLUE.executeFrame(GLUE.frames[0]);
-		}
-	}
-	if (GLUE.time > 1.5) GLUE.time = 1;
-}
-
-GLUE.sendPacket = function(type, pack) {
-	pack.t = type;
-	GLUE.packetsToSend.push(pack);
-}
-
-GLUE.lerp = function(val1, val2) {
-	return (val2-val1)*GLUE.time + val1;
-}
-
-GLUE.addPacketFunction = function(type, func) {
-	if (GLUE.packetFunctions.get(type) != undefined) {
-		console.log("packet function type " + type + " already in use.");
-		return;
-	}
-	GLUE.packetFunctions.set(type, func);
-}
-
-GLUE.getObjectFromID = function(id) {
-	for (var i = 0; i < GLUE.objects.length; i++) {
-		if (GLUE.object[i].id == id) {
-			return GLUE.objects[i];
-		}
-	}
-	return -1;
-}
-GLUE.getObjectIndexFromID = function(id) {
-	for (var i = 0; i < GLUE.objects.length; i++) {
-		if (GLUE.objects[i].id == id) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-GLUE.getInstanceFromID = function(id) {
-	for (var i = 0; i < GLUE.instances.length; i++) {
-		if (GLUE.instances[i].netObj.id == id) {
-			return GLUE.instances[i];
-		}
-	}
-	return -1;
-}
-GLUE.getInstanceIndexFromID = function(id) {
-	for (var i = 0; i < GLUE.instances.length; i++) {
-		if (GLUE.instances[i].netObj.id == id) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-GLUE.addObject = function(pack) {
-	GLUE.objects.push(pack);
-	if (GLUE.classes.get(pack.t) != undefined) {
-		GLUE.instances.push(new (GLUE.classes.get(pack.t))());
-		GLUE.instances[GLUE.instances.length - 1].oldNetObj = pack;
-		GLUE.instances[GLUE.instances.length - 1].netObj = pack;
-	}
-}
-
-GLUE.removeObject = function(pack) {
-	var index = GLUE.getObjectIndexFromID(pack.id);
-	if (index != -1) {
-		GLUE.objects.splice(index, 1);
-		
-		var index2 = GLUE.getInstanceIndexFromID(pack.id);
-		if (index2 != -1) {
-			GLUE.instances.splice(index2, 1);
-		}
-	}
-}
-
-GLUE.updateObjects = function(pack) {
-	for (var i = 0; i < pack.length; i++) {
-		var index = GLUE.getObjectIndexFromID(pack[i].id);
-		if (index != -1) {
-			GLUE.objects[index] = pack[i];
+		GLUE.ws.onopen = function() {
 			
-			var index2 = GLUE.getInstanceIndexFromID(pack[i].id);
+		}
+		GLUE.ws.onmessage = function(message) {
+			var data = JSON.parse(message.data);
+			
+			if (GLUE.frames.length < 1) {
+				GLUE.executeFrame(data.m);
+			}
+			GLUE.frames.push(data.m);
+			GLUE.frameTimes.push(data.t);
+		}
+	}
+
+	GLUE.executeFrame = function(messages) {
+		for (var i = 0; i < messages.length; i++) {
+			var func = GLUE.packetFunctions.get(messages[i].t);
+			if (func !== undefined)
+				func(messages[i].d);
+			else
+				console.log("Packet function for " + messages[i].t + " is undefined");
+		}
+	}
+
+	GLUE.update = function() {
+		GLUE.prevTime = GLUE.currentTime;
+		GLUE.currentTime = Date.now();
+		GLUE.dt = (GLUE.currentTime-GLUE.prevTime) / GLUE.updateTime;
+		
+		if (GLUE.packetsToSend.length > 0 && GLUE.ws.readyState == 1) {
+			GLUE.ws.send(JSON.stringify(GLUE.packetsToSend));
+			GLUE.packetsToSend = [];
+		}
+		
+		if (GLUE.frameTimes[0] == undefined || GLUE.frameTimes[1] == undefined)
+			return;
+		
+		var tick = (GLUE.currentTime - GLUE.prevTime) * GLUE.dt / (GLUE.frameTimes[1] - GLUE.frameTimes[0]);
+		GLUE.time += tick;
+		
+		if (GLUE.time >= 1.0) {
+			while (GLUE.frameTimes.length > 2) {
+				//advance to next frame
+				GLUE.frames.splice(0, 1);
+				GLUE.frameTimes.splice(0, 1);
+				GLUE.time -= 1.0;
+				//execute all of next frame's functions
+				GLUE.executeFrame(GLUE.frames[0]);
+			}
+		}
+		if (GLUE.time > 1.5) GLUE.time = 1;
+	}
+
+	GLUE.sendPacket = function(type, pack) {
+		pack.t = type;
+		GLUE.packetsToSend.push(pack);
+	}
+
+	GLUE.lerp = function(val1, val2) {
+		return (val2-val1)*GLUE.time + val1;
+	}
+
+	GLUE.addPacketFunction = function(type, func) {
+		if (GLUE.packetFunctions.get(type) != undefined) {
+			console.log("packet function type " + type + " already in use.");
+			return;
+		}
+		GLUE.packetFunctions.set(type, func);
+	}
+
+	GLUE.getObjectFromID = function(id) {
+		for (var i = 0; i < GLUE.objects.length; i++) {
+			if (GLUE.object[i].id == id) {
+				return GLUE.objects[i];
+			}
+		}
+		return -1;
+	}
+	GLUE.getObjectIndexFromID = function(id) {
+		for (var i = 0; i < GLUE.objects.length; i++) {
+			if (GLUE.objects[i].id == id) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	GLUE.getInstanceFromID = function(id) {
+		for (var i = 0; i < GLUE.instances.length; i++) {
+			if (GLUE.instances[i].netObj.id == id) {
+				return GLUE.instances[i];
+			}
+		}
+		return -1;
+	}
+	GLUE.getInstanceIndexFromID = function(id) {
+		for (var i = 0; i < GLUE.instances.length; i++) {
+			if (GLUE.instances[i].netObj.id == id) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	GLUE.addObject = function(pack) {
+		GLUE.objects.push(pack);
+		if (GLUE.classes.get(pack.t) != undefined) {
+			GLUE.instances.push(new (GLUE.classes.get(pack.t))());
+			GLUE.instances[GLUE.instances.length - 1].oldNetObj = pack;
+			GLUE.instances[GLUE.instances.length - 1].netObj = pack;
+		}
+	}
+
+	GLUE.removeObject = function(pack) {
+		var index = GLUE.getObjectIndexFromID(pack.id);
+		if (index != -1) {
+			GLUE.objects.splice(index, 1);
+			
+			var index2 = GLUE.getInstanceIndexFromID(pack.id);
 			if (index2 != -1) {
-				GLUE.instances[index2].oldNetObj = GLUE.instances[index2].netObj;
-				GLUE.instances[index2].netObj = pack[i];
+				GLUE.instances.splice(index2, 1);
 			}
 		}
 	}
-}
 
-GLUE.defineObject = function(type, cl) {
-	GLUE.classes.set(type, cl);
-}
+	GLUE.updateObjects = function(pack) {
+		for (var i = 0; i < pack.length; i++) {
+			var index = GLUE.getObjectIndexFromID(pack[i].id);
+			if (index != -1) {
+				GLUE.objects[index] = pack[i];
+				
+				var index2 = GLUE.getInstanceIndexFromID(pack[i].id);
+				if (index2 != -1) {
+					GLUE.instances[index2].oldNetObj = GLUE.instances[index2].netObj;
+					GLUE.instances[index2].netObj = pack[i];
+				}
+			}
+		}
+	}
 
-return GLUE;
+	GLUE.defineObject = function(type, cl) {
+		GLUE.classes.set(type, cl);
+	}
+
+	return GLUE;
 }
--- server/index.js
+++ server/index.js
@@ -1,175 +1,175 @@
 //GLUE.js server
 exports.Server = function(options={updateFPS: 60, packetFPS: 20}) {
-var server = require('uws').Server;
-var msgpack = require("msgpack-lite");
+	var server = require('uws').Server;
+	var msgpack = require("msgpack-lite");
 
-var GLUE = {wss: null, clients: [], objects: [], id: 0, packetFunctions: new Map()};
-GLUE.currTime = Date.now();
-GLUE.prevTime = 0;
-GLUE.dt = 0;
-GLUE.realTime = 0;
-GLUE.updateFPS = options.updateFPS;
-GLUE.packetFPS = options.packetFPS;
-GLUE.updateTime = (1/GLUE.updateFPS) * 1000;
-GLUE.packetTime = (1/GLUE.packetFPS) * 1000;
-
-function safelyParseJSON(message) {
-	var parsed;
-	
-	try {
-		parsed = msgpack.decode( new Uint8Array(message));
-	}
-	catch(e) {
-		
-	}
-	
-	return parsed;
-}
-
-GLUE.addObject = function(type, obj) {
-	obj.t = type;
-	GLUE.objects.push(obj);
-	obj.id = GLUE.id;
-	GLUE.id++;
-	GLUE.sendPacket("a", GLUE.getAddPacket(obj));
-	return obj;
-}
-GLUE.removeObject = function(obj) {
-	var index = GLUE.objects.indexOf(obj);
-	if (index != -1)
-		GLUE.objects.splice(index, 1);
-	GLUE.sendPacket("r", GLUE.getRemovePacket(obj));
-}
-
-GLUE.update = function() {
+	var GLUE = {wss: null, clients: [], objects: [], id: 0, packetFunctions: new Map()};
 	GLUE.currTime = Date.now();
-	GLUE.realTime = GLUE.currTime - GLUE.prevTime;
-	GLUE.dt = GLUE.realTime / GLUE.updateTime;
-	GLUE.prevTime = GLUE.currTime;
-	
-	for (var i = 0; i < GLUE.objects.length; i++) {
-		GLUE.objects[i].update(GLUE.dt);
-		if (GLUE.objects[i] != undefined && GLUE.objects[i].dead !== undefined && GLUE.objects[i].dead === true) {
-			GLUE.remove(GLUE.objects[i]);
-			i--;
-		}
-	}
-}
+	GLUE.prevTime = 0;
+	GLUE.dt = 0;
+	GLUE.realTime = 0;
+	GLUE.updateFPS = options.updateFPS;
+	GLUE.packetFPS = options.packetFPS;
+	GLUE.updateTime = (1/GLUE.updateFPS) * 1000;
+	GLUE.packetTime = (1/GLUE.packetFPS) * 1000;
 
-GLUE.packetUpdate = function() {
-	var objs = [];
-	for (var i = 0; i < GLUE.objects.length; i++) {
-		var pack = GLUE.getUpdatePacket(GLUE.objects[i]);
-		if (pack !== undefined)
-			objs.push(pack);
-	}
-	if (objs.length > 0)
-		GLUE.sendPacket("u", objs);
-	
-	for (var i = 0; i < GLUE.clients.length; i++) {
-		if (GLUE.clients[i].readyState == 1 && GLUE.clients[i].packetsToSend.length > 0) {
-			var frame = GLUE.clients[i].packetsToSend;
-			GLUE.clients[i].send(msgpack.encode(frame));
-			GLUE.clients[i].packetsToSend = [];
-		}
-	}
-}
-
-GLUE.sendPacket = function(type, data, client) {
-	var pack = {t: type, d: data};
-	if (client == undefined) {
-		for (var i = 0; i < GLUE.clients.length; i++) {
-			GLUE.clients[i].packetsToSend.push(pack);
-		}
-	}
-	else {
-		client.packetsToSend.push(pack);
-	}
-}
-
-GLUE.addPacketFunction = function(type, func) {
-	GLUE.packetFunctions.set(type, func);
-}
-
-GLUE.getAddPacket = function(obj) {
-	var pack = {};
-	if (obj.addPacket !== undefined)
-		pack = obj.addPacket();
-	
-	pack = GLUE.roundFloatsInObj(pack);
-	pack.id = obj.id;
-	pack.t = obj.t;
-	return pack;
-}
-GLUE.getRemovePacket = function(obj) {
-	var pack = {};
-	if (obj.removePacket !== undefined)
-		pack = obj.removePacket();
-	
-	pack = GLUE.roundFloatsInObj(pack);
-	pack.id = obj.id;
-	pack.t = obj.t;
-	return pack;
-}
-GLUE.getUpdatePacket = function(obj) {
-	var pack = {};
-	if (obj.updatePacket !== undefined)
-		pack = obj.updatePacket();
-	else
-		return undefined;
-	
-	pack = GLUE.roundFloatsInObj(pack);
-	pack.id = obj.id;
-	pack.t = obj.t;
-	return pack;
-}
-
-GLUE.roundFloatsInObj = function(pack) {
-	for (var key in pack) {
-		if (Number(pack[key]) === pack[key] && pack[key] % 1 !== 0) {
-			pack[key] = Math.floor(pack[key] * 100);
-			pack[key] /= 100;
-		}
-	}
-	return pack;
-}
-
-GLUE.start = function(port=8443) {
-	GLUE.wss = new server({port: port});
-	setInterval(GLUE.update, GLUE.updateTime);
-	setInterval(GLUE.packetUpdate, GLUE.packetTime);
-	console.log("GLUE listening on port "+port+"...");
-	
-	GLUE.wss.on('connection', function(ws) {
-		ws.packetsToSend = [];
-		GLUE.clients.push(ws);
-		for (var i = 0; i < GLUE.objects.length; i++) {
-			GLUE.sendPacket("a", GLUE.getAddPacket(GLUE.objects[i]), ws);
-		}
-		GLUE.onConnect(ws);
+	function safelyParseJSON(message) {
+		var parsed;
 		
-		ws.on('message', function(m) {
-			var messages = safelyParseJSON(m);
-			for (var i = 0; i < messages.length; i++) {
-				var func = GLUE.packetFunctions.get(messages[i].t);
-				func(messages[i], ws);
-			}
-		});
-		ws.on('close', function() {
-			GLUE.onDisconnect(ws);
-			var index = GLUE.clients.indexOf(ws);
-			if (index != -1)
-				GLUE.clients.splice(index, 1);
-			ws.terminate();
-		});
-	});
-}
+		try {
+			parsed = msgpack.decode( new Uint8Array(message));
+		}
+		catch(e) {
+			
+		}
+		
+		return parsed;
+	}
 
-GLUE.onConnect = function(socket) {
-	
-}
-GLUE.onDisconnect = function(socket) {
-	
-}
-return GLUE;
+	GLUE.addObject = function(type, obj) {
+		obj.t = type;
+		GLUE.objects.push(obj);
+		obj.id = GLUE.id;
+		GLUE.id++;
+		GLUE.sendPacket("a", GLUE.getAddPacket(obj));
+		return obj;
+	}
+	GLUE.removeObject = function(obj) {
+		var index = GLUE.objects.indexOf(obj);
+		if (index != -1)
+			GLUE.objects.splice(index, 1);
+		GLUE.sendPacket("r", GLUE.getRemovePacket(obj));
+	}
+
+	GLUE.update = function() {
+		GLUE.currTime = Date.now();
+		GLUE.realTime = GLUE.currTime - GLUE.prevTime;
+		GLUE.dt = GLUE.realTime / GLUE.updateTime;
+		GLUE.prevTime = GLUE.currTime;
+		
+		for (var i = 0; i < GLUE.objects.length; i++) {
+			GLUE.objects[i].update(GLUE.dt);
+			if (GLUE.objects[i] != undefined && GLUE.objects[i].dead !== undefined && GLUE.objects[i].dead === true) {
+				GLUE.remove(GLUE.objects[i]);
+				i--;
+			}
+		}
+	}
+
+	GLUE.packetUpdate = function() {
+		var objs = [];
+		for (var i = 0; i < GLUE.objects.length; i++) {
+			var pack = GLUE.getUpdatePacket(GLUE.objects[i]);
+			if (pack !== undefined)
+				objs.push(pack);
+		}
+		if (objs.length > 0)
+			GLUE.sendPacket("u", objs);
+		
+		for (var i = 0; i < GLUE.clients.length; i++) {
+			if (GLUE.clients[i].readyState == 1 && GLUE.clients[i].packetsToSend.length > 0) {
+				var frame = GLUE.clients[i].packetsToSend;
+				GLUE.clients[i].send(msgpack.encode(frame));
+				GLUE.clients[i].packetsToSend = [];
+			}
+		}
+	}
+
+	GLUE.sendPacket = function(type, data, client) {
+		var pack = {t: type, d: data};
+		if (client == undefined) {
+			for (var i = 0; i < GLUE.clients.length; i++) {
+				GLUE.clients[i].packetsToSend.push(pack);
+			}
+		}
+		else {
+			client.packetsToSend.push(pack);
+		}
+	}
+
+	GLUE.addPacketFunction = function(type, func) {
+		GLUE.packetFunctions.set(type, func);
+	}
+
+	GLUE.getAddPacket = function(obj) {
+		var pack = {};
+		if (obj.addPacket !== undefined)
+			pack = obj.addPacket();
+		
+		pack = GLUE.roundFloatsInObj(pack);
+		pack.id = obj.id;
+		pack.t = obj.t;
+		return pack;
+	}
+	GLUE.getRemovePacket = function(obj) {
+		var pack = {};
+		if (obj.removePacket !== undefined)
+			pack = obj.removePacket();
+		
+		pack = GLUE.roundFloatsInObj(pack);
+		pack.id = obj.id;
+		pack.t = obj.t;
+		return pack;
+	}
+	GLUE.getUpdatePacket = function(obj) {
+		var pack = {};
+		if (obj.updatePacket !== undefined)
+			pack = obj.updatePacket();
+		else
+			return undefined;
+		
+		pack = GLUE.roundFloatsInObj(pack);
+		pack.id = obj.id;
+		pack.t = obj.t;
+		return pack;
+	}
+
+	GLUE.roundFloatsInObj = function(pack) {
+		for (var key in pack) {
+			if (Number(pack[key]) === pack[key] && pack[key] % 1 !== 0) {
+				pack[key] = Math.floor(pack[key] * 100);
+				pack[key] /= 100;
+			}
+		}
+		return pack;
+	}
+
+	GLUE.start = function(port=8443) {
+		GLUE.wss = new server({port: port});
+		setInterval(GLUE.update, GLUE.updateTime);
+		setInterval(GLUE.packetUpdate, GLUE.packetTime);
+		console.log("GLUE listening on port "+port+"...");
+		
+		GLUE.wss.on('connection', function(ws) {
+			ws.packetsToSend = [];
+			GLUE.clients.push(ws);
+			for (var i = 0; i < GLUE.objects.length; i++) {
+				GLUE.sendPacket("a", GLUE.getAddPacket(GLUE.objects[i]), ws);
+			}
+			GLUE.onConnect(ws);
+			
+			ws.on('message', function(m) {
+				var messages = safelyParseJSON(m);
+				for (var i = 0; i < messages.length; i++) {
+					var func = GLUE.packetFunctions.get(messages[i].t);
+					func(messages[i], ws);
+				}
+			});
+			ws.on('close', function() {
+				GLUE.onDisconnect(ws);
+				var index = GLUE.clients.indexOf(ws);
+				if (index != -1)
+					GLUE.clients.splice(index, 1);
+				ws.terminate();
+			});
+		});
+	}
+
+	GLUE.onConnect = function(socket) {
+		
+	}
+	GLUE.onDisconnect = function(socket) {
+		
+	}
+	return GLUE;
 }
